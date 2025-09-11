b2c47589d804e8696b472c553524bbad3f7055e0
vitor-2006 <vitorbalbinotdellatorre@gmail.com>
2025-09-10 21:22:43 -0300

    conexão com banco de dados

--- src/config/env.ts
+++ src/config/env.ts
@@ -8,5 +8,11 @@ dotenv.config({
 
 export const config = {
     app_name: process.env.APP_NAME,
-    app_port: process.env.APP_PORT
+    app_port: process.env.APP_PORT,
+
+    db_host: process.env.DB_HOST,
+    db_port: process.env.DB_PORT,
+    db_database: process.env.DB_DATABASE,
+    db_user: process.env.DB_USER,
+    db_password: process.env.DB_PASSWORD
 }
---
+++ src/database/database.ts
@@ -0,0 +1,50 @@
+import { Pool, PoolConfig, Query, QueryResult, Result } from "pg"
+import { config } from "../config/env" 
+import { Database as DatabaseResult } from "../interfaces/database.interface"
+
+class Database
+{
+    private pool: Pool
+
+    constructor()
+    {
+        const poolConfig: PoolConfig = {
+            host: config.db_host,
+            port: Number(config.app_port),
+            database: config.db_database,
+            user: config.db_user,
+            password: config.db_password,
+            ssl: {
+                rejectUnauthorized: false
+            },
+            max: 20,
+            idleTimeoutMillis: 30000
+        }
+        this.pool = new Pool(poolConfig)
+    }
+    async query(sql: string, values?: any[]): Promise<DatabaseResult>
+    {
+        try {
+            const result: QueryResult = await this.pool.query(sql, values)
+
+            return {
+                status: true,
+                data: result.rows
+            }
+        } catch(error) {
+            return {
+                status: false,
+                data: [],
+                error: error as Error
+            }
+        }
+    }
+    async end(): Promise<void>
+    {
+        await this.pool.end()
+        console.log('conexão com banco de dados encerrada')
+    }
+}
+
+const database = new Database
+export { database }
