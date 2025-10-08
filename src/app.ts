eac8edf435a2d1da2e18cf098941d078eb78ff8f
vitor-2006 <vitorbalbinotdellatorre@gmail.com>
2025-10-08 19:00:04 -0300

    adicionando CID

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,10 @@ import { routes } from './routes/routes'
 import cors from 'cors'
 import { log } from "./middleware/log.middleware"
 import { errorHandling } from './middleware/error-handling.middleware'
-
+import { cid } from './middleware/cid.middleware'
 const app: Express = express()
 
+app.use(cid)
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -16,9 +17,9 @@ app.use(routes)
 
 import { database } from './database/database';
 
-(async () => {
-    console.log(await database.query('select now() as data_atual', []));
-})();
+// (async () => {
+//     console.log(await database.query('select now() as data_atual', []));
+// })();
 
 app.use(errorHandling)
 export { app }
--- src/controllers/example.controllers.ts
+++ src/controllers/example.controllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 
 class ExampleController {
     async helloWorld(req: Request, res: Response){
+        console.log('cid', req.headers.cid)
         return res.send('Hello Typescrypt')
     }
     testError(req: Request, res: Response){
